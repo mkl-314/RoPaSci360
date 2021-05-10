3e1870a6e04fc4a49d4dcf813852ec74384804e9
Ming Kim Low <mingkiml@student.unimelb.edu.au>
2021-05-10 12:18:27 +1000

    Updated ML function

--- MachineLearning.py
+++ MachineLearning.py
@@ -10,10 +10,12 @@ def ml(state, future_state):
         # my_invincible, op invincible
 
         current_score = tokens_on_board(state) + tokens_in_hand(state)
-        on_board = tokens_on_board(state) 
-        in_hand = tokens_in_hand(state)
+        my_on_board = len(state.data[state.me])
+        op_on_board = len(state.data[state.opponent])
+        my_in_hand = state.tokens_in_hand[state.me]
+        op_in_hand = state.tokens_in_hand[state.opponent]
 
         future_score = tokens_on_board(future_state) + tokens_in_hand(future_state)
-        filewriter.writerow([str(current_score), str(on_board), str(in_hand), str(future_score)])
+        filewriter.writerow([current_score, my_on_board, op_on_board, my_in_hand, op_in_hand, future_score])
 
 
